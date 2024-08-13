a0ee617d83940509662a62dd8feb29893c43f05d
Dablouan Gonçalves <gdblouan@gmail.com>
2024-08-12 22:23:21 -0300

    comecei o rodape

--- src/componentes/rodape/index.tsx
+++ src/componentes/rodape/index.tsx
@@ -1,20 +1,27 @@
 function Rodape(){
     return(
         <div className="Rodape">
-            <label>
-                <ul> 
-                    <li>link aqui</li>
-                    
-                </ul>
-                <ul>
-                    <li>Link aqui</li>
-                </ul>
-                <ul>
-                    <li>Link aqui</li>
-                </ul>
-            </label>
-
-
+            <table className="TabelaRodape">
+                <tr className="tituloTabela">
+                    <th>Duvidas</th>
+                    <th>Informações</th>
+                    <th>Conato</th>
+                </tr>
+                <tr className="conteudoTabela">
+                    <td>Sei lá oq</td>
+                    <td>Sei lá oq</td>
+                    <td>Sei lá oq</td>
+                </tr>
+                <tr className="conteudoTabela2">
+                    <td>Sei lá oq</td>
+                    <td>Sei lá oq</td>
+                    <td>Sei lá oq</td>
+                </tr>
+            </table>
+            <div className="conecte-se">
+                Conecte-se com a gente
+                <div><img src=" " alt="" /></div>
+            </div>
         </div>
     )
 }
