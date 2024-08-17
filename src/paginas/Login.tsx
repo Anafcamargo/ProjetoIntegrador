20548b2d57f1007cd5b666860ebbdf78098505c9
Vinicius <vinicius.asciello@senacsp.edu.br>
2024-08-16 21:58:30 -0300

    Criação da página login

--- src/paginas/Login.tsx
+++ src/paginas/Login.tsx
@@ -1,15 +1,56 @@
-import Cabecalho from "../componentes/cabecalho";
-
+import { Link } from "react-router-dom";
 
 function Login(){
     return(
+<div>
+
+  <div className="container-login">
+    
+
+
+     <div className="container-login-box">
+     
+
+      <h2>Login</h2>
+      <form>
+           <div className="input-group">
+           <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" placeholder="Digite seu e-mail aqui" /> 
+          </div>
+           
+           <div className="input-group">
+           <label htmlFor="password">Senha</label>
+          <input type="password" id="password" placeholder="Digite sua senha aqui" /> 
+           </div>
+
+           
+
+      <button type="submit" className="login-button">Login</button>
+
+      </form>
+
+          <div className="login-footer">
+          <a href="#" className="forgot-password">Esqueci minha senha</a>
+          <p>Não tem uma conta? <Link to='/Cadastro'>Cadastre-se</Link></p>
+          </div>     
+         
+         
+
         <div>
-
-    <Cabecalho />
-        AQUI É A TELA DE LOGIN 
-        
-
+           
+        <Link to='/'><button className="back-button">Voltar</button></Link>
         </div>
+          
+     </div>
+      
+      
+  
+
+  
+  </div>
+ 
+
+</div>
     )
 }
 
