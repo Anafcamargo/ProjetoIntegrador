54527d5edf5e9747cdc77151606ab3b8bb6c8073
Nickzinsm <jonaskakashi13@gmail.com>
2024-08-23 22:07:17 -0300

    reecriação da pagina de feedbakcs

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import './estilo/estilo.css'
-import Home from './paginas/home';
-import {Routes, Route} from 'react-router-dom';
-import Login from './paginas/Login';
-import Notfound from './paginas/notfound';
-import Cadastro from './paginas/cadastro';
-import Tela_Usuario from './paginas/tela+usuario';
-import Tela_Voluntario from './paginas/tela_voluntario';
+import React from "react";
+import "./estilo/estilo.css";
+import Home from "./paginas/home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./paginas/Login";
+import Notfound from "./paginas/notfound";
+import Cadastro from "./paginas/cadastro";
+import Tela_Usuario from "./paginas/tela+usuario";
+import Tela_Voluntario from "./paginas/tela_voluntario";
+import { register } from "swiper/element/bundle";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+register();
 
 function App() {
- 
-  return(
+  return (
     <div>
-       
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Login/Cadastro' element={<Cadastro/>}/>
-        <Route path='*' element={<Notfound/>} />
-        <Route path='/Cadastro' element={<Cadastro/>}/>
-        <Route path='/Usuario' element={<Tela_Usuario/>}/>
-        <Route path='/Voluntario' element={<Tela_Voluntario/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Login/Cadastro" element={<Cadastro />} />
+        <Route path="*" element={<Notfound />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
+        <Route path="/Usuario" element={<Tela_Usuario />} />
+        <Route path="/Voluntario" element={<Tela_Voluntario />} />
       </Routes>
-  
-    </div> 
-   )
- 
-  
- 
-
+    </div>
+  );
 }
-
-   
 
 export default App;
