9757e563f4903fabed64ee6557336152043fb076
Viniciuse <[vinicius.asciello@senacsp.edu.br]>
2024-09-18 19:41:28 -0300

    ajuste do ranking

--- src/paginas/home.tsx
+++ src/paginas/home.tsx
@@ -97,7 +97,7 @@ function Home() {
 
       {/*--------Tela Ranking--------*/}
       <div className="container-ranking">
-      
+      Ranking de Voluntários
       
       <Swiper
   slidesPerView={2}
