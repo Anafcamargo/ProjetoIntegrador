b70ae85a5820fcd8a09cf6cd3859ca66a3e59d8e
Ana <af.bauru16@gmail.com>
2024-09-18 21:57:09 -0300

    alterações tela voluntario,resolver Ranking

--- src/paginas/tela_voluntario.tsx
+++ src/paginas/tela_voluntario.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Cabecalho2 from '../componentes/cabecalho2';
+import Rodape from '../componentes/rodape';
 import '../estilo/estilo.css';
 
 // Interface do chamado
@@ -34,17 +35,34 @@ function TelaVoluntario() {
     );
   };
 
+   
+
   return (
     <><Cabecalho2 />
     
     <div className="suporte-container">
       {/* Perfil do usuário à esquerda */}
+
       <div className="profile-section">
         <h2>Perfil</h2>
+        <br />
+        <img className='fotoperfil' src="OIP.png" alt=""
+            width= '70px' height='70px' />
+            <br />
+
         <div className="profile-details">
           <p>Nome: João da Silva</p>
+          <p>CPF: 123.456.789-01</p>
+          <p>Data de nascimento: 01/12/2001</p>
           <p>Email: joao.silva@suporte.com</p>
+          <p>Telefone: 14 98888-9999</p>
+          <p>Endereço: R. vista</p>
+          <p>Número: 54-86</p>
+          <p>Bairro: Jardim Boa vista</p>
         </div>
+        <br />
+        
+        
       </div>
 
       {/* Chamados disponíveis */}
@@ -78,7 +96,7 @@ function TelaVoluntario() {
           <p>Você ainda não aceitou nenhum chamado.</p>
         )}
       </div>
-    </div></>
+    </div> <Rodape/></>
   );
 }
 
