37bb23101229e7c83b38b30ca5926032d73ccd82
Dablouan Gonçalves <gdablouan@gmail.com>
2024-09-18 21:30:30 -0300

    aterado forms para divs

--- src/paginas/cadastro.tsx
+++ src/paginas/cadastro.tsx
@@ -47,61 +47,83 @@ function Cadastro() {
 
         {tipoCadastro === "usuario" && (
           <div className="formContainerUsuario">
-            <form>
+            <div>
+              
               <div className="formGroup">
                 <label className="label">Nome Completo</label>
                 <input type="text" placeholder="Digite seu nome completo" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">Telefone</label>
                 <input type="text" placeholder="Digite seu número de WhatsApp" className="input" />
               </div>
+
+              <div className="formGroup">
+                <label className="label">Senha</label>
+                <input type="text" placeholder="Digite sua nova senha" className="input" />
+              </div>
+
               <button type="submit" className="button">
                 Criar conta
               </button>
-            </form>
+            </div>
           </div>
         )}
 
         {tipoCadastro === "voluntario" && (
           <div className="formContainer">
-            <form>
+            <div>
+              
               <div className="formGroup">
                 <label className="label">Nome Completo</label>
                 <input type="text" placeholder="Digite seu nome completo" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">CPF</label>
                 <input type="text" placeholder="Digite seu CPF" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">Data de nascimento</label>
                 <input type="date" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">E-mail</label>
                 <input type="email" placeholder="Digite seu e-mail" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">Telefone</label>
                 <input type="text" placeholder="Digite seu telefone" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">Endereço</label>
                 <input type="text" placeholder="Digite seu endereço" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">Número</label>
                 <input type="text" placeholder="Número" className="input" />
               </div>
+              
               <div className="formGroup">
                 <label className="label">Bairro</label>
                 <input type="text" placeholder="Digite seu bairro" className="input" />
               </div>
+
+              <div className="formGroup">
+                <label className="label">Senha</label>
+                <input type="text" placeholder="Digite sua nova senha" className="input" />
+              </div>
+
               <button type="submit" className="button">
                 Criar conta
               </button>
-            </form>
+            </div>
           </div>
         )}
 
